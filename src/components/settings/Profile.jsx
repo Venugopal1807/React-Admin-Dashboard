981ae55759c149cf80ecea1ad8e209e89a949b96
venu gopal <kunchepu.venugopal@gmail.com>
2025-04-18 09:54:05 +0530

    first commit

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src='https://marketplace.canva.com/EAFEits4-uw/1/0/1600w/canva-boy-cartoon-gamer-animated-twitch-profile-photo-oEqs2yqaL8s.jpg'
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>Venu Gopal</h3>
+					<p className='text-gray-400'>kunchepu.venugopal@gmail.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Edit Profile
+			</button>
+		</SettingSection>
+	);
+};
+export default Profile;
